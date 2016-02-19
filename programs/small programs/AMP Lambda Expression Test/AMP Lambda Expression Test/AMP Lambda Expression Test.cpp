cca36ef1ae4cc99c9df69ce9b0c16bba9179987b
Dugin13 <niels@lichking.dk>
2016-02-19 13:52:51 +0100

    made temp result more dynamic

--- programs/small programs/AMP Lambda Expression Test/AMP Lambda Expression Test/AMP Lambda Expression Test.cpp
+++ programs/small programs/AMP Lambda Expression Test/AMP Lambda Expression Test/AMP Lambda Expression Test.cpp
@@ -12,7 +12,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	const int Size1d = Size*Size;
 	const int Size_Of_input = 2;
 	const int number_Of_Functions = 1;
-	const int number_Of_Temp_result = 3;
+	const int number_Of_Temp_result = 1; // must not be zero even know there is no temp result!!!
 	int input[Size_Of_input][Size1d];
 	int A[Size1d];
 	int B[Size1d];
@@ -42,19 +42,34 @@ int _tmain(int argc, _TCHAR* argv[])
 	func.push_back(0); // -> output
 
 
+
+
+
 	int *p_input = &input[0][0];
 	concurrency::array_view<int, 2> GPU_input(Size_Of_input, Size1d, p_input);
 	concurrency::array_view<int, 1> GPU_output(Size1d, C);
-	concurrency::array_view<int, 2> GPU_Func(number_Of_Functions, 4, func);
-	//concurrency::array_view<int, 2> GPU_Func(3,4, A);
 	GPU_output.discard_data();
+	
+	concurrency::array_view<int, 2> GPU_Func(number_Of_Functions, 4, func);
+	
+	std::vector<int> Temp_result;
+	for (i = 0; i < Size1d; i++)
+	{
+		for (x = 0; x < number_Of_Temp_result; x++)
+		{
+			Temp_result.push_back(0);
+		}
+	}
+	concurrency::array_view<int, 2> GPU_Temp_result(Size1d, number_Of_Temp_result, Temp_result);
+	GPU_Temp_result.discard_data();
+	
+	//concurrency::array_view<int, 2> GPU_Func(3,4, A)
 
 
 
 	auto GPU_code = [=](concurrency::index<1> idx) restrict(amp)
 	{
 		int i = idx[0];
-		int temp_result_holder[3]; // number_Of_Temp_result
 		for (int x = 0; x < number_Of_Functions; x++)
 		{
 			// finding which imput to use
@@ -68,7 +83,7 @@ int _tmain(int argc, _TCHAR* argv[])
 			else
 			{
 				int temp_index = (GPU_Func[x][0] * -1) - 1;
-				input_1 = temp_result_holder[temp_index];
+				input_1 = GPU_Temp_result[i][temp_index];
 			}
 			// for input 2
 			if (GPU_Func[x][2] > 0)
@@ -79,24 +94,24 @@ int _tmain(int argc, _TCHAR* argv[])
 			else
 			{
 				int temp_index = (GPU_Func[x][2] * -1) - 1;
-				input_2 = temp_result_holder[temp_index];
+				input_2 = GPU_Temp_result[i][temp_index];
 			}
 			int temp_result=0;
 
 			// finding which operator to used
-			if (GPU_Func[x][1] = 1 ) // (+)
+			if (GPU_Func[x][1] == 1 ) // (+)
 			{
 				temp_result = input_1 + input_2;
 			}
-			else if (GPU_Func[x][1] = 2) // (-)
+			else if (GPU_Func[x][1] == 2) // (-)
 			{
 				temp_result = input_1 - input_2;
 			}
-			else if (GPU_Func[x][1] = 3) // (*)
+			else if (GPU_Func[x][1] == 3) // (*)
 			{
 				temp_result = input_1 * input_2;
 			}
-			else if (GPU_Func[x][1] = 4) // (/)
+			else if (GPU_Func[x][1] == 4) // (/)
 			{
 				temp_result = input_1 / input_2;
 			}
@@ -112,7 +127,7 @@ int _tmain(int argc, _TCHAR* argv[])
 			else // place the result in temp_result_holder
 			{
 				int temp_index = (GPU_Func[x][3] * -1) - 1;
-				temp_result_holder[temp_index] = temp_result;
+				GPU_Temp_result[i][temp_index] = temp_result;
 			}
 		}
 	};
